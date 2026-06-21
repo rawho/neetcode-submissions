aab6d925c0179943fd7043b579d07fff57f2caa6
Rahul T <rahulmanojcoder@gmail.com>
2026-06-21 23:47:22 +0530

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int minLength = nums.length + 1;
+        int sum = 0;
+        for(int right = 0; right < nums.length; right++){
+            sum += nums[right];
+            while(sum >= target){
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        if(minLength == nums.length + 1){
+            return 0;
+        } else {
+            return minLength;
+        }
+    }
+}
